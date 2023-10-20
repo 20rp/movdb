dc614b18d8dc6d9082a9d76cc2062122bee11242
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2023-10-21 11:43:55 +1300

    Added sql build script

---
+++ db/create-db.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE IF NOT EXISTS movdb;
+USE DATABASE movdb;
+
+CREATE TABLE movies (
+    id int,
+    movieTitle varchar(50),
+    
+)
+
+CREATE TABLE movieGenres (
+
+)
+
+CREATE TABLE genres (
+
+)
+
+CREATE TABLE movieStudios (
+
+)
+
+CREATE TABLE studios (
+
+)
