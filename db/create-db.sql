49cc61c0acc5fa1b3f4dbb27f78a41576246f464
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2023-11-25 22:32:55 +1300

    Added SQL build script.

--- db/create-db.sql
+++ db/create-db.sql
@@ -1,24 +1,44 @@
 CREATE DATABASE IF NOT EXISTS movdb;
-USE DATABASE movdb;
+USE movdb;
 
 CREATE TABLE movies (
-    id int,
+    ID int NOT NULL,
     movieTitle varchar(50),
-    
+    movieDate DATE,
+    moviePlatform varchar(50),
+    genreID int,
+    studioID int,
+    PRIMARY KEY (ID)
 )
 
 CREATE TABLE movieGenres (
-
+    ID int,
+    fk_movieID int,
+    fk_genreID int,
+    PRIMARY KEY (ID),
+    ADD FOREIGN KEY (fk_movieID) REFERENCES movies(ID),
+    ADD FOREIGN KEY (fk_genreID) REFERENCES genres(ID)
 )
 
 CREATE TABLE genres (
-
+    ID int,
+    genreName varchar(100),
+    PRIMARY KEY (ID)
 )
 
 CREATE TABLE movieStudios (
+    ID int,
+    fk_movieID int,
+    fk_studioID int,
+    PRIMARY KEY (ID),
+    ADD FOREIGN KEY (fk_movieID) REFERENCES movies(ID),
+    ADD FOREIGN KEY (fk_studioID) REFERENCES studios(ID)
 
 )
 
 CREATE TABLE studios (
-
+    ID int,
+    studioName varchar(100),
+    studioCountry varchar(100),
+    PRIMARY KEY (ID)
 )
