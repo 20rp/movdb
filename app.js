0027c11d022bd9366e95b3c94bf3cf24ef560e3d
Arlo Wotherspoon <awotherspoon@eit.ac.nz>
2023-12-11 18:03:09 +1300

    Got express working and created basic index

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+app.set("views", path.join(__dirname, "pages"))
+app.set("view engine", "pug")
+
 app.get('/', (req, res) => {
-    res.send('Hello world')
+    // TODO: Homepage
+    res.render('index')
+})
+
+app.get('/insert', (req, res) => {
+    // TODO: insert-controller
+})
+
+app.get('/movies', (req, res) => {
+    // TODO: movies table
 })
 
 app.listen(port, () => {
